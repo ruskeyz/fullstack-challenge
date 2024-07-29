77d6ac289f6295ca8f8e868d21ea75a9c797869c
ruskeyz <lutayilya@gmail.com>
2024-07-29 22:07:46 +0100

    server report handler

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,52 @@
 import express from "express";
 import { json } from "body-parser";
 import cors from "cors";
+import pg from "pg";
+import { values } from "regenerator-runtime";
 
 const createApp = () => {
+  const pool = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "novata",
+    password: "example",
+    port: 5555,
+  });
   const app = express();
   app.use(json());
   app.use(
     cors({
       origin: "*",
       methods: ["GET", "POST"],
-    })
+    }),
   );
 
-  app.get("/user", (req, res) => {
-    res.send({ data: "user" });
+  // create new report
+  app.post("/report", async (req, res) => {
+    const { title, body } = req.body;
+    const { rows } = await pool.query(
+      `
+INSERT INTO reports (title, description) VALUES ($1, $2)
+`,
+      [title, body],
+    );
+
+    res.json({ rows });
   });
-  app.post("/user", (req, res) => {
-    res.send({ data: "user" });
+  // Add metric to report
+  app.put(`/report/:id`, async (req, res) => {
+    const { id, data } = req.body;
+    try {
+      const query = {
+        text: `UPDATE reports SET data = jsonb_set(data, '{metrics}', data -> 'metrics' || $2) WHERE uuid = $1
+`,
+        values: data,
+      };
+      const { rows } = await pool.query(query);
+      res.json({ rows });
+    } catch (err) {
+      res.status(404).json(`Not found ${err.message}`);
+    }
   });
 
   return app;
